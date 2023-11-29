0dbfd64d73d84d9a9aa8d2592281277009bbf144
Tr1sm3gist0 <128314429+Tr1sm3gist0@users.noreply.github.com>
2023-11-29 16:32:07 +0000

    Update install-03.sh

--- install-03.sh
+++ install-03.sh
@@ -13,7 +13,7 @@ cp -r 7h0seF1L3s/swappy ~/.config/swappy
 #cp .config/nvim/init.vim ~/.config/nvim/init.vim
 cp -r 7h0seF1L3s/val/templates ~/.config/val/templates
 ## Set pywal ##
-wal -i ~/P4nic-Arch/wallpapers/01.jpg
+wal -i ~/Downloads/w4llp4p3rs/1.jpg
 ## OMZSH ##
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo "Finished! Do a sudo reboot."
