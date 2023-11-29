3111c1ec64ea1af4d61fb784392e71928b5b1db8
Tr1sm3gist0 <128314429+Tr1sm3gist0@users.noreply.github.com>
2023-11-29 18:48:44 +0000

    Update install-01.sh

--- install-01.sh
+++ install-01.sh
@@ -2,6 +2,7 @@
 ## Set Dark theme ##
 mkdir ~/.config/gtk-3.0
 touch ~/.config/gtk-3.0/settings.ini
+echo "gtk-application-prefer-dark-theme=1" > ~/.config/gtk-3.0/settings.ini
 ## Removes Dolphin and dunst
 sudo pacman -Rs
 ## Creates Home directories
