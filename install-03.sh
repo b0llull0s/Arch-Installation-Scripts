0de4b3835acdb8a14c62d9067f3339512154b533
b0llull0s <128314429+b0llull0s@users.noreply.github.com>
2024-03-20 05:30:22 +0000

    Update install-03.sh

--- install-03.sh
+++ install-03.sh
@@ -5,8 +5,8 @@ git clone https://github.com/b0llull0s/7h0seF1L3s
 mv w4llp4p3rs ~/Downloads/
 ## Dot Files ##
 cp -r 7h0seF1L3s/alacritty ~/.config/
-cp -r 7h0seF1L3s/kitty ~/.config/kitty
-cp -r 7h0seF1L3s/hypr ~/.config/hypr
+cp -r 7h0seF1L3s/kitty ~/.config/
+cp -r 7h0seF1L3s/hypr ~/.config/
 cp -r 7h0seF1L3s/waybar ~/.config/waybar
 cp -r 7h0seF1L3s/rofi ~/.config/rofi
 cp -r 7h0seF1L3s/swappy ~/.config/swappy
