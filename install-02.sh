ae5194300206d0bb5f693155adcaaa2613803c60
b0llull0s <128314429+b0llull0s@users.noreply.github.com>
2024-03-21 15:32:19 +0000

    Update install-02.sh

--- install-02.sh
+++ install-02.sh
@@ -86,7 +86,7 @@ cp 7h0seF1L3s/.zshrc ~/
 #cp .config/nvim/init.vim ~/.config/nvim/init.vim
 cp -r 7h0seF1L3s/val/templates ~/.config/val/templates
 ## Set pywal ##
-wal -i ~/Downloads/w4llp4p3rs/01.jpg
+wal -i ~/Downloads/w4llp4p3rs/1.jpg
 ## Script Permissions
 cd ~/.config/hyper/scripts
 sudo chmod +x *.sh
