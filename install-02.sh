cd75e603e656da6326bb5ebc2409524e36be7607
Isaac <128314429+b0llull0s@users.noreply.github.com>
2024-08-06 12:27:57 +0100

    Add files via upload

---
+++ install-02.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+set -e
+
+# Functions
+
+error_exit() {
+    echo "$1" 1>&2
+    exit 1
+}
+
+install_packages() {
+    for package in "$@"; do
+        if ! pacman -Q "$package" &>/dev/null; then
+            sudo pacman -Sy --noconfirm "$package" || error_exit "Failed to install $package"
+        fi
+    done
+}
+
+enable_services() {
+    for service in "$@"; do
+        sudo systemctl enable --now "$service" || error_exit "Failed to enable $service"
+    done
+}
+
+# Packages
+
+install_packages nmap ncat openbsd-netcat mariadb 
+
+# Yay Packages
+
+yay -S --noconfirm rustscan
+yay -S --noconfirm ffuf
+yay -S --noconfirm burpsuite
+yay -S --noconfirm feroxbuster
+yay -S --noconfirm naabu
+
+# Snap
+
+sudo snap install john-the-ripper
