546a534fac692c682490f1ee1aa5c573ae5ca3ee
Nick Dandolos <128314429+b0llull0s@users.noreply.github.com>
2025-01-21 21:48:23 +0000

    Update install-02.sh

--- install-02.sh
+++ install-02.sh
@@ -39,7 +39,7 @@ enable_services() {
 }
 
 # Packages
-install_packages nmap ncat openbsd-netcat rlwrap mariadb john feroxbuster impacket metasploit exploitdb proxychains-ng oath-toolkit bind sqlmap wpscan
+install_packages nmap ncat openbsd-netcat rlwrap mariadb john feroxbuster impacket metasploit exploitdb proxychains-ng oath-toolkit bind sqlmap wpscan sqlite
 # Python
 install_packages python-pwntools python-aiosmtpd python-websocket-client python-bs4
 
